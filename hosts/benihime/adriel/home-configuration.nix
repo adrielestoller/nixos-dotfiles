f4a6594c007ecc96dc27603d0e64824e557a884c
Adriel Estoller <adrielestoller@gmail.com>
2025-05-29 20:24:19 -0300

    Change nixos configs to home-manager

---
+++ hosts/benihime/adriel/home-configuration.nix
@@ -0,0 +1,35 @@
+{ config, inputs, lib, pkgs, ... }:
+
+{
+  imports = [
+    # ../../modules/
+  ];
+
+  home = {
+    username = "adriel";
+    homeDirectory = "/home/${config.home.username}";
+    packages = with pkgs; [
+      kitty
+      discord
+      obsidian
+      steam
+      gimp
+      inkscape
+    ];
+   
+    sessionVariables = {
+      BROWSER = lib.getExe pkgs.firefox;
+      EDITOR = lib.getExe pkgs.vim;
+      TERMINAL = lib.getExe pkgs.kitty;
+    };
+    
+    stateVErsion = "25.05";
+  };
+
+  programs = {
+    bash.enable = true;
+    firefox.enable = true;
+
+    home-manager.enable = true; 
+  };
+}
---
+++ hosts/benihime/configuration.nix
@@ -0,0 +1,112 @@
+{ config, pkgs, lib, inputs, ... }:
+
+{
+  imports = [
+    ./hardware-configuration.nix
+    inputs.home-manager.nixosModules.default
+  ];
+
+  boot = {
+    loader = {
+      systemd-boot.enable = true;
+      efi.canTouchEfiVariables = true;
+    };
+  };
+
+  hardware.graphics = {
+    enable = true;
+    extraPackages = with pkgs; [
+      intel-compute-runtime
+      intel-media-driver
+      intel-vaapi-driver
+    ];
+  };
+
+  networking = {
+    hostName = "benihime";
+    nameservers = [
+      "1.0.0.1"
+      "1.1.1.1"
+    ];
+    networkmanager = {
+      enable = true;
+      dns = "none";
+      wifi.powersave = false;
+  };
+
+  time.timeZone = "America/Recife";
+
+  i18n = {
+    defaultLocale = "pt_BR.UTF-8";
+    extraLocaleSettings = {
+      LC_ADDRESS = "pt_BR.UTF-8";
+      LC_IDENTIFICATION = "pt_BR.UTF-8";
+      LC_MEASUREMENT = "pt_BR.UTF-8";
+      LC_MONETARY = "pt_BR.UTF-8";
+      LC_NAME = "pt_BR.UTF-8";
+      LC_NUMERIC = "pt_BR.UTF-8";
+      LC_PAPER = "pt_BR.UTF-8";
+      LC_TELEPHONE = "pt_BR.UTF-8";
+      LC_TIME = "pt_BR.UTF-8";
+    };
+  };
+
+  services = {
+    xserver = {
+      enable = true;
+
+      displayManager.lightdm.enable = true;
+      desktopManager.cinnamon.enable = true;
+
+      xkb = {
+        layout = "br";
+        variant = "";
+      };
+    };
+    
+    printing.enable = true;
+
+    pulseaudio.enable = false;
+    pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+      jack.enable = true;
+      wireplumber.enable = true;
+    };
+  };
+
+  console.keyMap = "br-abnt2";
+
+  security.rtkit.enable = true;
+
+  users.users.adriel = {
+    isNormalUser = true;
+    description = "Adriel Estoller";
+    extraGroups = [ "networkmanager" "wheel" "audio" "video" ];
+  };
+
+  programs.firefox.enable = true;
+
+  nixpkgs.config.allowUnfree = true;
+
+  environment.systemPackages = with pkgs; [
+     vim
+     git
+     github-cli
+  ];
+
+  system.stateVersion = "25.05"; 
+  
+  nix = {
+    package = pkgs.nixVersions.git;
+
+    settings = {
+       auto-optimise-store = true;
+       experimental-features = ["nix-command" "flakes"];
+       trusted-users = ["@wheel"];
+    };
+  };
+
+}
