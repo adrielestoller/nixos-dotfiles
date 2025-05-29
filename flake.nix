a138adcbe0a077201db9580c05ae47c70b9d181f
Adriel Estoller <adrielestoller@gmail.com>
2025-05-29 20:23:10 -0300

    Add home-manager and change files structure

--- flake.nix
+++ flake.nix
@@ -2,19 +2,49 @@
   description = "NixOS Adriel's flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.05";
-    nixpkgsunstable.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs-edge.url = "github:nixos/nixpkgs";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, nixpkgsunstable, ... }@inputs: {
+  outputs = { self, nixpkgs, home-manager, ... }@inputs:
+  let
+    systems = [
+      "aarch64-darwin"
+      "aarch64-linux"
+      "i686-linux"
+      "x86_64-darwin"
+      "x86_64-linux"
+    ];
+
+    eachPkgs = nixpkgs.lib.genAttrs systems (
+      system:
+        import nixpkgs {
+          inherit overlays system;
+          config.allowUnfree = true;
+        }
+    );
+  in {
     
     nixosConfigurations.benihime = nixpkgs.lib.nixosSystem {
       specialArgs = { inherit inputs; };
       modules = [
-        ./configuration.nix
+        ./hosts/benihime/configuration.nix
+	{
+            nixpkgs.pkgs = eachPkgs."x86_64-linux";
+        }
       ];
+    };
+
+    homeConfigurations = {
+      "adriel@benihime" = home-manager.lib.homeManagerConfiguration {
+        extraSpecialArgs = { inherit inputs; };
+        pkgs =  eachPkgs."x86_64-linux";
+        modules = [./hosts/benihime/adriel/home-configuration.nix];
+      };
     };
   };
 }
